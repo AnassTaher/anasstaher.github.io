b5785782d6a8505cae5e5c373e05cebf0c561320
Anass Taher <AnassTaher2003@hotmail.com>
2023-06-30 22:54:52 +0200

    etch-a-sketch is (basically) finished, might improve

--- etch-a-sketch/index.js
+++ etch-a-sketch/index.js
@@ -1,15 +1,80 @@
 
+const inputs = document.querySelectorAll('input');
+const buttons = document.querySelectorAll('button');
+let rows = document.getElementById('rows').value;
+let columns = document.getElementById('columns').value;
+let color = document.getElementById('color').value;
+let backgroundColor = "white";
+let mouseDown = false;
 
 
-const inpColor = document.getElementById('color');
-const inpRow = document.getElementById('row');
-const inpCol = document.getElementById('col');
-const eraser = document.getElementById('eraser');
-const clear = document.getElementById('clear');
 
-console.log(inpColor);
-
-inpColor.addEventListener("input", (e) => {
+const handleItem = (e) => {
+  if (e.type === 'mouseover' && !mouseDown) return;
   console.log(e);
-  console.log(e.target.value);
+  console.log(color);
+  e.target.style.backgroundColor = color;
+};
+
+const drawGrid = () => {
+  const grid = document.getElementById('grid');
+  grid.innerHTML = "";
+  for(let i = 0; i < rows * columns; i++) {
+    const item = document.createElement('div');
+    item.classList.add('grid-item');
+    item.addEventListener('mousedown', handleItem);
+    item.addEventListener('mouseover', handleItem);
+    grid.appendChild(item);
+  }
+  grid.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+};
+
+const handleInput = (e) => {
+  if(e.target.value === "") return;
+  const val = e.target.value;
+
+  switch(e.target.id){
+    case "rows":
+      rows = val;
+      break;
+    case "columns":
+      columns = val;
+      break;
+    case "color":
+      color = val; 
+      break;
+    default:
+      break;
+  }
+
+  drawGrid();
+};
+
+const handleButton = (e) => {
+
+  if(e.target.id === "clear") {
+    drawGrid();
+  } else if(e.target.id === "eraser"){
+    console.log("eraser");
+    (color === backgroundColor) ? color = document.getElementById('color').value : color = backgroundColor;
+  }
+
+};
+
+
+drawGrid();
+
+Array.prototype.forEach.call(inputs, child => {
+  child.addEventListener("input", handleInput);
 });
+
+Array.prototype.forEach.call(buttons, child => {
+  child.addEventListener("click", handleButton);
+});
+
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
+
+
